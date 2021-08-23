888627a89441e54adbe4e66830e52d2b9be9b66b
alisonfilice <alison.f.smith@gmail.com>
2021-08-23 16:07:43 +0000

    methods exercises

---
+++ methods/methods.exercises.rb
@@ -0,0 +1,12 @@
+def greeting(name)
+  "Hello, " + name + "!"
+end
+
+puts greeting("Alison")
+
+def multiply(a, b)
+  a * b
+end
+
+puts multiply(3, 100)
+
---
+++ methods/say.rb
@@ -0,0 +1,12 @@
+def say(words ='hello')
+  puts words + "."
+end
+
+say()
+say("hi")
+say("how are you")
+say("I'm fine")
+
+
+
+
